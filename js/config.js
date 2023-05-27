8823ebe5cda15e35ff7fc5e2c67de6f5a3ba4bb7
binilvjacob <itsmebinilvjacob@gmail.com>
2023-05-27 10:03:59 +0530

    config gitignore

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 export const DISTRIBUTION_BASE_BACKEND_URL="https://campaign.admint.io";
 export const COLOR_SCHEME_BASE_URL="https://admint-io.github.io/ftdeditor";
-//export const NFT_IMAGE_BASE_URL="https://admint-io.github.io/ftdeditor";
+//export const NFT_IMAGE_BASE_URL="https://admint-io.github.io/ftdeditor";cxcxc
